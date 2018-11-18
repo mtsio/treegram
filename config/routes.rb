e22f89e9daf5e0fe18842e571b609b3b1f58d1c1
Michail Tsiolkas <mtsio@cryptolab.net>
2018-11-18 15:50:55 +0200

    Update routes for new model

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get '/' => 'home#index'
+  resources :users do
+    resources :photos
+  end
+  
+  resources :tags, only: [:create, :destroy]
+  get '/log-in' => "sessions#new"
+  post '/log-in' => "sessions#create"
+  get '/log-out' => "sessions#destroy", as: :log_out
+
+  resources :relations
+end
