19c73967c9cf9e04acf9f93942b1c5b13ee3f913
Nikolaos Zois <nikoszoisse@gmail.com>
2018-11-20 00:06:22 +0200

    Add comments controller model database

---
+++ db/migrate/20181119220044_add_fields_to_comments.rb
@@ -0,0 +1,5 @@
+class AddFieldsToComments < ActiveRecord::Migration
+  def change
+    add_column :comments, :content, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181119213926) do
+ActiveRecord::Schema.define(version: 20181119220044) do
 
   create_table "comments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "content"
   end
 
   create_table "photos", force: :cascade do |t|
