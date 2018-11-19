d1e9b2e5694827d5e6af947d8a17a4c8fbd935ff
Nikolaos Zois <nikoszoisse@gmail.com>
2018-11-19 23:43:11 +0200

    Add comment controller, and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181119202834) do
+ActiveRecord::Schema.define(version: 20181119213926) do
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "photos", force: :cascade do |t|
     t.integer  "user_id"
