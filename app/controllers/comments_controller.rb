39f8a8f943595798fe4219bd360f5fb07dc37ef6
Nikolaos Zois <nikoszoisse@gmail.com>
2018-11-20 00:54:36 +0200

    Add comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+
+  def create
+    @user = User.find(params[:comment][:user_id])
+    @photo = Photo.find(params[:comment][:photo_id])
+    @comment = Comment.create({user_id: params[:comment][:user_id], photo_id: params[:comment][:photo_id]})
+    flash[:notice]="User #{@user.email} added comment for photo #{@photo.title} at #{@comment.created_at}"
+    redirect_to user_path(session[:user_id])
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ActiveRecord::Base
     belongs_to :user
-    belongs_to :author
-    
+    belongs_to :photo
 end
