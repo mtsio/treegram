e746cc50f47fdd5de36a70b9cbe759659b53aadf
Michail Tsiolkas <mtsio@cryptolab.net>
2018-11-21 01:37:12 +0200

    display comments for every photo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,8 +6,22 @@ class CommentsController < ApplicationController
   def create
     @user = User.find(params[:comment][:user_id])
     @photo = Photo.find(params[:comment][:photo_id])
-    @comment = Comment.create({user_id: params[:comment][:user_id], photo_id: params[:comment][:photo_id]})
-    flash[:notice]="User #{@user.email} added comment for photo #{@photo.title} at #{@comment.created_at}"
+    @comment = Comment.create({user_id: params[:comment][:user_id],
+                               photo_id: params[:comment][:photo_id],
+                               content: params[:comment][:content]}) unless
+      params[:comment][:content].blank?
+    
+    if params[:comment][:content].blank?
+      flash[:alert]="Empty Comment"
+    elsif
+      flash[:notice]="User #{@user.email} added comment for photo \
+                           #{@photo.title} at #{@comment.created_at}"
+    end
     redirect_to user_path(session[:user_id])
+  end
+
+  private
+  def comment_params
+    params.require([:comment][:content])
   end
 end
