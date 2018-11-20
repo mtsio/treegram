a7f604ceb27675c65af369bd638cbdf03d884f1f
Nikolaos Zois <nikoszoisse@gmail.com>
2018-11-21 00:37:03 +0200

    add missing param field at comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
   def create
     @user = User.find(params[:comment][:user_id])
     @photo = Photo.find(params[:comment][:photo_id])
-    @comment = Comment.create({user_id: params[:comment][:user_id], photo_id: params[:comment][:photo_id]})
+    @comment = Comment.create({user_id: params[:comment][:user_id], photo_id: params[:comment][:photo_id], content: params[:comment][:content]})
     flash[:notice]="User #{@user.email} added comment for photo #{@photo.title} at #{@comment.created_at}"
     redirect_to user_path(session[:user_id])
   end
