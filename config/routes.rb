b5549b765f7d8049bc188f375828561cbcb48902
Nikolaos Zois <nikoszoisse@gmail.com>
2018-12-24 00:15:14 +0200

    Add slides route and do make the show html to have link to the slides we want.

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,14 @@ Rails.application.routes.draw do
     resources :photos
     resources :relations
     resources :comments
+    member do
+      get 'slides'
+    end
   end
-  
+
   resources :tags, only: [:create, :destroy]
   resources :comments, only: [:create, :destroy]
+
   get '/log-in' => "sessions#new"
   post '/log-in' => "sessions#create"
   get '/log-out' => "sessions#destroy", as: :log_out
